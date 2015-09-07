8b05c2c433c6be8ab0c1effff9ac5217c41233c7
Faris Chebib <faris@zaneprep.com>
2015-09-07 14:10:50 -0600

    updated to simpler user model

--- apps/pastryio/settings/base.py
+++ apps/pastryio/settings/base.py
@@ -42,7 +42,7 @@ THIRD_PARTY_APPS = (
 
 LOCAL_APPS = (
     'pastryio',
-    'users',
+    'profiles',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
---
+++ apps/profiles/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from django.contrib.auth.models import (
+    User
+)
+
+
+class BaseProfile(models.Model):
+    user = models.OneToOneField(User)
+    avatar = models.ImageField(_("avatar"), blank=True)
+
+    def __unicode__(self):
+        return self.user.username
