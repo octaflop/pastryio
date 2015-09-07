6599da63f6bcb406bf81049d1061b53ebe704deb
Faris Chebib <faris@zaneprep.com>
2015-09-07 15:22:44 -0600

    initial commit

--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -1,17 +1,33 @@
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import ugettext_lazy as _
+from django.core.urlresolvers import reverse
+
 from pastryio.models.mixins import ArchiveMixin
+from users.models import BaseProfile
 
 
 class Ingredient(ArchiveMixin):
-    name = models.CharField(max_length=254)
-    calories = models.IntegerField(blank=True)
+    name = models.CharField(_("Name"), max_length=254)
+    calories = models.IntegerField(_("Calories"), blank=True)
 
 
 class Recipe(ArchiveMixin):
-    title = models.CharField(max_length=254)
+    title = models.CharField(_("Title"), max_length=254)
+    author = models.ForeignKey(BaseProfile)
+    ingredients = models.ManyToManyField(Ingredient)
+    photo = models.ImageField(_("Photo"), blank=True)
+
+    @property
+    def slug(self):
+        return slugify(self.title)
+
+    # def get_absolute_url(self):
+        # return reverse("")
 
 
 class RecipeStep(ArchiveMixin):
-    step = models.PositiveIntegerField()
-    description = models.TextField()
+    step = models.PositiveIntegerField(_("Step"))
+    description = models.TextField(_("Description"))
     recipe = models.ForeignKey(Recipe)
+    photo = models.ImageField(_("Photo"), blank=True)
