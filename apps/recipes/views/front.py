07895bc78dd9c857e8a9a83a8ab5b2e94e437ab2
Faris Chebib <faris@zaneprep.com>
2015-09-08 11:19:37 -0600

    updated context_object_name

--- apps/recipes/views/front.py
+++ apps/recipes/views/front.py
@@ -6,6 +6,7 @@ from recipes.models import Ingredient
 class IngredientListView(ListView):
     template_name = "recipes/front/index.html"
     model = Ingredient
+    context_object_name = "ingredients"
 
 
 class IngredientDetailView(DetailView):
