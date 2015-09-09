5b6a07787a279ed8b8bbc352a5098458effc7923
Faris Chebib <faris@zaneprep.com>
2015-09-09 09:02:48 -0600

    updated to use admin mixins

--- apps/recipes/admin.py
+++ apps/recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
+from pastryio.admin.mixins import ArchiveAdminMixin
 from recipes.models import Recipe, Ingredient
 
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
+admin.site.register(Recipe, ArchiveAdminMixin)
+admin.site.register(Ingredient, ArchiveAdminMixin)
