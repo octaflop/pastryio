8672ef5a7b43566ba49faf2efd6b9c4883f69675
Faris Chebib <faris@zaneprep.com>
2015-09-11 13:56:54 -0600

    building sending example

--- apps/pastryio/static/pastryio/js/main.js
+++ apps/pastryio/static/pastryio/js/main.js
@@ -3,7 +3,7 @@
 angular.module("pastryio", ['ngWebSocket'])
   .factory('wsData', function($websocket) {
     // Open a WebSocket connection
-    var dataStream = $websocket('wss://127.0.0.1:9000/');
+    var dataStream = $websocket('wss://127.0.0.1:9000/room-test/');
 
     var collection = [];
 
@@ -24,4 +24,8 @@ angular.module("pastryio", ['ngWebSocket'])
     var blog = this;
 
     blog.wsData = wsData;
+
+    blog.sendClicked = function sendClicked(){
+      wsData.send(blog.msg);
+    };
   });
